e4db09548839cff409f5763173b08f60313f3ffb
pike4 <pike4@purdue.edu>
2016-09-25 05:27:59 -0400

    Kind of finished? The acceleration data doesn't really reflect what's actually happening, but at least the sphere is moving around its 5:30

--- netCode.js
+++ netCode.js
@@ -1,3 +1,4 @@
+var users;
 function sendHttpGetRequest (url, outString, callback)
 {
 	var xmlHttp = new XMLHttpRequest();
@@ -6,22 +7,36 @@ function sendHttpGetRequest (url, outString, callback)
 	{
 		if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
 		{
-			callback(xmlHttp.responseText);
+			return callback(xmlHttp.responseText);
 		}
 	}
 	
 	xmlHttp.open("GET", url, true);
-	xmlHttp.send(outString);
+	xmlHttp.send();
 };
 
 function sendDownloadRequest()
 {
-	sendHttpGetRequest("http://38.110.21.194:8081", "DOWNLOAD", function(responseText){
-		console.log(responseText);
+	
+	return sendHttpGetRequest("http://38.110.21.194:8081/DOWNLOAD", "", function(responseText){
+		//console.log(responseText);
+		
+		var DL_Data = responseText.toString().split(",");
+		
+		for(var i = 0; i < DL_Data.length/4; i++)
+		{
+			var name = DL_Data[4 * i];
+			
+			var dat = [DL_Data[(4*i) + 1], DL_Data[(4*i) + 2], DL_Data[(4*i) + 3]];
+			
+			users.set(name, dat);
+		}
+		//console.log(users);
 	});
 };
 
 function startRepeatingGet()
 {
-	var continuousRequest = setInterval(sendDownloadRequest, 1000);
+	
+	var continuousRequest = setInterval(sendDownloadRequest, 100);
 };
